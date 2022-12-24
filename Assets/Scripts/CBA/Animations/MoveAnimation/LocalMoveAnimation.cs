16ab78cb3b1d3e312a815773cfd5411cdaffb26d
VasylKhreptak <vasylkhreptak12@gmail.com>
2022-12-24 21:40:41 +0200

    Added recording to LocalMoveAnimation;

--- Assets/Scripts/CBA/Animations/MoveAnimation/LocalMoveAnimation.cs
+++ Assets/Scripts/CBA/Animations/MoveAnimation/LocalMoveAnimation.cs
@@ -31,28 +31,57 @@ namespace CBA.Animations.MoveAnimation
 
 #if UNITY_EDITOR
 
+        [ShowNonSerializedField] private bool _isRecording;
+
         [Button("Assign Start Position")]
         private void AssignStartPositionVariable()
         {
+            if (_isRecording) return;
+
             _startPosition = _transform.localPosition;
         }
 
         [Button("Assign Target Position")]
         private void AssignTargetPosition()
         {
+            if (_isRecording) return;
+
             _targetPosition = _transform.localPosition;
         }
 
         [Button("Move To Start")]
         private void MoveToStartPositionEditor()
         {
+            if (_isRecording) return;
+
             MoveToStartState();
         }
 
         [Button("Move To End")]
         private void MoveToTargetPosition()
         {
-            _transform.localPosition = _targetPosition;
+            if (_isRecording) return;
+
+            MoveToEndState();
+        }
+
+        [Button("Start Recording")]
+        private void StartRecording()
+        {
+            _startPosition = _transform.localPosition;
+
+            _isRecording = true;
+        }
+
+        [Button("Stop Recording")]
+        private void StopRecording()
+        {
+            if (_isRecording == false) return;
+
+            _targetPosition = _transform.localPosition;
+            MoveToStartState();
+
+            _isRecording = false;
         }
 
         private void OnDrawGizmosSelected()
@@ -61,11 +90,33 @@ namespace CBA.Animations.MoveAnimation
 
             if (_transform == null || parent == null) return;
 
+            if (_isRecording)
+            {
+                DrawRecordingArrow(parent);
+            }
+            else
+            {
+                DrawDefaultArrow(parent);
+            }
+        }
+
+        private void DrawDefaultArrow(Transform parent)
+        {
             Vector3 startPosition = parent.TransformPoint(_startPosition);
             Vector3 targetPosition = parent.TransformPoint(_targetPosition);
             Vector3 direction = targetPosition - startPosition;
 
             Gizmos.color = Color.white;
+            Extensions.Gizmos.DrawArrow(startPosition, direction);
+        }
+
+        private void DrawRecordingArrow(Transform parent)
+        {
+            Vector3 startPosition = parent.TransformPoint(_startPosition);
+            Vector3 direction = _transform.position - startPosition;
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(startPosition, 0.1f);
             Extensions.Gizmos.DrawArrow(startPosition, direction);
         }
 
