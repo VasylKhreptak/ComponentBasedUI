bf7611f315c18f7509592ba194dfbc542ecef8df
VasylKhreptak <vasylkhreptak12@gmail.com>
2022-12-30 19:51:53 +0200

    Added Global Animation Actions;

--- Assets/Scripts/TEST.cs
+++ Assets/Scripts/TEST.cs
@@ -1,13 +1,15 @@
+using DG.Tweening;
 using UnityEngine;
 using Sequence = DG.Tweening.Sequence;
 
 public class TEST : MonoBehaviour
 {
     [Header("References")]
-    [SerializeField] private RectTransform _rectTransform;
-
+    [SerializeField] private Component _component;
+    
     private void Awake()
     {
         Sequence sequence;
+        Tween tween;
     }
 }
