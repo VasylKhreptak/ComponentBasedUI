a6f5f88b5ad5fcbd151c217d8eb840345058ff4c
VasylKhreptak <vasylkhreptak12@gmail.com>
2022-12-30 21:53:12 +0200

    Added some management actions;

--- Assets/Scripts/TEST.cs
+++ Assets/Scripts/TEST.cs
@@ -1,15 +1,6 @@
-using DG.Tweening;
+using System.Diagnostics;
 using UnityEngine;
-using Sequence = DG.Tweening.Sequence;
 
 public class TEST : MonoBehaviour
 {
-    [Header("References")]
-    [SerializeField] private Component _component;
-    
-    private void Awake()
-    {
-        Sequence sequence;
-        Tween tween;
-    }
 }
